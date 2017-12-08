2174eaf74bb4904bda7ffec84eb9d4816b471589
Tarun Mehta <tmehta813@gmail.com>
2017-12-08 16:43:42 +0530

    Adding Playground file.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let startIndex = 0
+let endIndex = 3
+for i in startIndex..<endIndex {
+    print(i)
+    
+    print("str")
+    print(str)
+    print("startIndex")
+    print(startIndex)
+    print("end")
+    print(endIndex)
+    
+}
+
+//
+//printPermutationOfString(str:"01232", startIndex:0,endIndex:3)
+//
+//func printPermutationOfString(str:String, startIndex:Int, endIndex:Int) {
+//    
+//    if startIndex == endIndex {
+////        count += 1;
+////        print(count);
+//        print(str)
+//    } else {
+//            }
+//}
